3af2e1ae2123d3a8185b5574734a632a6c13dc5d
Enderson Quiroz <endequirozca@cesde.net>
2024-09-26 20:42:38 -0500

    implementacion de un recorrido a un  arreglo con nombres

--- script.js
+++ script.js
@@ -8,10 +8,19 @@
 // console.log(otroArreglo)
 
 
-let numeros = [1,2,3,4,5,6,7,8,9,10]
-for(let index = 0; index < numeros.length; index++){
-//console.log(numeros[index])
-if (numeros [index] % 2 == 0){
-    console.log(numeros[index]*4)
+// let numeros = [1,2,3,4,5,6,7,8,9,10]
+// for(let index = 0; index < numeros.length; index++){
+// //console.log(numeros[index])
+// if (numeros [index] % 2 == 0){
+//     console.log(numeros[index]*4)
+// }
+// }
+
+
+let nombres = []
+for(let index = 0; index < 5; index++){
+    nombres[index] = prompt('ingrese su nombre: ')
 }
+for(let index = 0; index < nombres.length; index++) {
+    console.log(nombres[index])
 }
