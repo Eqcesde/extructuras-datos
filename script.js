41acbcb186f00a76a85788fd322fc8b4354001e2
Enderson Quiroz <endequirozca@cesde.net>
2024-09-26 20:17:15 -0500

    implementacion de un recorridoca un arreglo de numeros

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-let numero = 23 //variable 
-let variasCosas = [23, true, 'Enderson', function(){}, [], numero]
-let otroArreglo= [] // arreglo vacío
+// let numero = 23 //variable 
+// let variasCosas = [23, true, 'Enderson', function(){}, [], numero]
+// let otroArreglo= [] // arreglo vacío
 
-console.log(variasCosas[0] * variasCosas[5])
-console.log(variasCosas[5])
-console.log(variasCosas[3])
-console.log(otroArreglo)
+// console.log(variasCosas[0] * variasCosas[5])
+// console.log(variasCosas[5])
+// console.log(variasCosas[3])
+// console.log(otroArreglo)
+
+
+let numeros = [1,2,3,4,5,6,7,8,9,10]
+for(let index = 0; index < numeros.length; index++){
+//console.log(numeros[index])
+if (numeros [index] % 2 == 0){
+    console.log(numeros[index]*4)
+}
+}
